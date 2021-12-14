30d725a0e7fba7b2da7517539453db368e0c34d5
Osama Soliman <prof.o.soliman@gmail.com>
2021-12-14 22:55:35 +0200

    init

---
+++ js/app.js
@@ -0,0 +1,61 @@
+const clock = document.querySelector("#clock");
+const footer = document.querySelector(".footer");
+const getClockTime = () => {
+  var now = new Date();
+  var hour = now.getHours();
+  var minute = now.getMinutes();
+  var second = now.getSeconds();
+  var ap = "AM";
+  if (hour > 11) {
+    ap = "PM";
+  }
+  if (hour > 12) {
+    hour = hour - 12;
+  }
+  if (hour == 0) {
+    hour = 12;
+  }
+  if (hour < 10) {
+    hour = "0" + hour;
+  }
+  if (minute < 10) {
+    minute = "0" + minute;
+  }
+  if (second < 10) {
+    second = "0" + second;
+  }
+  var timeString = hour + ":" + minute + ":" + second + " " + ap;
+  return timeString;
+}
+
+setInterval(function () {
+  clock.innerHTML = getClockTime();
+}, 1000);
+
+
+const toggleFullScreen = () => {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    }
+  }
+}
+
+const toggleFooter = () => {
+  if (footer.style.display === "none") {
+    footer.style.display = "block";
+  } else {
+    footer.style.display = "none";
+  }
+}
+
+document.addEventListener(
+  "mousedown",
+  function (e) {
+    toggleFullScreen();
+    toggleFooter();
+  },
+  false
+);
